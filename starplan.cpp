c4c18b5d7f6c048a4b21268ebbab4d1372d2f136
zhaoxiangfei <wingsnorth@hotmail.com>
2019-08-27 16:18:58 +0800

    fix: fix tbcurbigplans emplace obj[]

--- starplan.cpp
+++ starplan.cpp
@@ -38,7 +38,7 @@ void starplan::init()
         obj.start_time              = get_head_block_time();;
         obj.end_time                = 0;
     });
-    tbcurbigplans.emplace(sender_id,[&](auto obj){
+    tbcurbigplans.emplace(sender_id,[&](auto &obj){
         obj.index           = tbcurbigplans.available_primary_key();
 		obj.bigplanets      = {};
 		obj.rwdplanets      = {};
@@ -677,7 +677,7 @@ void starplan::createBigPlanet(uint64_t sender)
         obj.create_round    = currentRound();
     });
     auto itor = tbcurbigplans.find(currentRound());
-    tbcurbigplans.modify(itor,sender,[&](auto obj){
+    tbcurbigplans.modify(itor,sender,[&](auto &obj){
 		obj.bigplanets.push_back(sender);
     });
 }
@@ -1063,7 +1063,7 @@ void starplan::createNewRound()
         obj.end_time                = 0;
     });
 
-    tbcurbigplans.emplace(sender,[&](auto obj){
+    tbcurbigplans.emplace(sender,[&](auto &obj){
         obj.index           = tbcurbigplans.available_primary_key();
 		obj.bigplanets      = {};
 		obj.rwdplanets      = {};
